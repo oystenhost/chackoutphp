2aac63381a821515d5f69110bca3b78f39e61e83
oystenhost <116473354+oystenhost@users.noreply.github.com>
2023-02-01 17:43:43 +0530

    Create chackout.php

---
+++ chackout.php
@@ -0,0 +1,76 @@
+<div class="min-h-screen flex justify-center items-center bg-gray-700">
+    <div class="h-auto w-96 bg-white rounded-lg p-3 ">
+        <div class="main ">
+            <div class="relative mt-2">
+                <p class="text-xl font-semibold">Checkout</p>
+                <p class="text-sm text-pink-500 absolute top-1 left-24 font-semibold">#40950</p>
+                <p class="mt-1 text-pink-400 ">Diverse and rich experience,thye existing structure of the organization contruibutes to the prepration.</p>
+                <hr class="mt-4">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <p class="text-pink-900 font-semibold">Plan Without Tax</p>
+                <p>$68.00</p>
+            </div>
+            <div class="mt-4 flex justify-between">
+                <p class="text-pink-900 font-semibold">VAT(20%)</p>
+                <p>$13.60</p>
+            </div>
+            <hr class="mt-4">
+            <div class="mt-4 flex justify-between">
+                <p class="text-pink-900 text-lg font-semibold">Total</p>
+                <p class="text-lg ">$81.60</p>
+            </div>
+            <button class="buy h-12 mb-8 w-full bg-gray-400 rounded-lg text-sm cursor-pointer text-white hover:bg-gray-600 transition-all mt-12">Buy Now</button>
+        </div>
+        <div class="main hidden">
+            <div class="relative mt-2">
+                <p class="text-xl text-center font-semibold">Billing Info</p>
+                <i class="back absolute top-2 text-gray-500 cursor-pointer fa fa-arrow-left"></i>
+            </div>
+            <div class="w-full mt-8 flex gap-2 ">
+                <div class="w-full relative">
+                    <input  class="h-12 border-2 rounded-lg outline-none px-3 text-sm w-full focus:border-blue-500 transition-all" type="text" >
+                    <span class="absolute -top-6 text-sm left-0">Company Name</span>
+                </div>
+                <div class="w-full relative">
+                    <input  class=" h-12 border-2 rounded-lg outline-none px-3 text-sm w-full focus:border-blue-500 transition-all" type="number" require>
+                    <span class="absolute -top-6 text-sm left-0">VAT</span>
+                </div>
+            </div>
+            <div class="w-full relative mt-9">
+                <input  class=" h-12 border-2 pr-16 rounded-lg outline-none px-3 text-sm w-full focus:border-blue-500 transition-all" type="text">
+                <span class="absolute -top-6 text-sm left-0">Cardholder Name</span>
+            </div>
+            <div class="w-full relative mt-9">
+                <input  class=" h-12 border-2 pr-16 rounded-lg outline-none px-3 text-sm w-full focus:border-blue-500 transition-all" type="text" placeholder="0000 0000 0000 0000" data-slots="0" data-accept="\d" size="19" require>
+                <span class="absolute -top-6 text-sm left-0">Card Number</span>
+                <span class="h-10 w-10 absolute right-2 top-1 cursor-pointer"><img src="https://imgur.com/5SjlNJf.jpg"></span>
+            </div>
+            <div class="w-full mt-9 flex gap-2 ">
+                <div class="w-full relative">
+                    <input  class="h-12 border-2 rounded-lg outline-none px-3 text-sm w-full focus:border-blue-500 transition-all" type="text"  placeholder="mm/yyyy" data-slots="my" require> 
+                    <span class="absolute -top-6 text-sm left-0">Expiry Date</span>
+                </div>
+                <div class="w-full relative">
+                    <input  class=" h-12 border-2 rounded-lg outline-none px-3 text-sm w-full focus:border-blue-500 transition-all" type="text"  placeholder="000" data-slots="0" data-accept="\d" size="3" require>
+                    <span class="absolute -top-6 text-sm left-0">CVV</span>
+                </div>
+            </div>
+            <button class="proceed h-12 mb-8 w-full bg-blue-400 rounded-lg text-sm cursor-pointer text-white hover:bg-blue-600 transition-all mt-12">Proceed Payment</button>
+        </div>
+        <div class="main hidden">
+            <div class="relative mt-2">
+                <p class="text-xl text-center text-green-400 font-semibold">Success<i class="text-green-400 ml-1 fa fa-check"></i></p>
+                <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                    <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+                    <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+                </svg>
+                <p class="text-center text-green-800 mt-4 font-semibold mb-10 ">Thanks for purchasing,your item has been added to cart and will be delivered shortly,hope you felt good in dealing with us.</p>
+            </div>
+            
+        </div>
+        
+        
+    </div>
+    
+</div>
